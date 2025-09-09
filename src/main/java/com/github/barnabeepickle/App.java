022e4f7af4d814f7452c21bf83c32e41948801ca
barnabeepickle <linusrk30@gmail.com>
2025-09-09 12:34:01 -0400

    implemented functionality

--- src/main/java/com/github/barnabeepickle/App.java
+++ src/main/java/com/github/barnabeepickle/App.java
@@ -1,13 +1,51 @@
 package com.github.barnabeepickle;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws IOException, ParseException
     {
-        System.out.println( "Hello World!" );
+        // create Options object
+        Options options = new Options();
+        options.addOption("path", true, "The file path to list, default = current dir.");
+        
+        // create Parser
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        
+        // get the value of the path option
+        String fileDirectory = cmd.getOptionValue("path");
+        
+        // check if it was used so we can asign a default if needed
+        String directoryPath = null;
+        if(fileDirectory == null) {
+            directoryPath = System.getProperty("user.dir");
+        }
+        else {
+            directoryPath = fileDirectory;
+        }
+
+        // create a Path object for the directory
+        Path directory = Paths.get(directoryPath);
+
+        // use DirectoryStream to list files in the directory
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+          
+          // use a for loop to print them on the command line
+            for (Path file : stream) {
+                System.out.println(file.getFileName());
+            }
+        }
     }
 }
