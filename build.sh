8e5cc5cccd469024cb4418b55f4f7149f9dcf005
barnabeepickle <linusrk30@gmail.com>
2025-09-10 10:27:52 -0400

    added functionality to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "This is where a script for building this project goes."
+echo "Cleaning with Maven"
+mvn clean
+echo "Compiling with Maven"
+mvn compile
+echo "Assembling with Maven"
+mvn assembly:single
+echo "Done, you should find your built file in the root of the /target directory."
+# the single line for doing this is:
+# mvn clean compile assembly:single
